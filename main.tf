a848498a165dbe1bad3d4b626a4d3e686297383c
unknown <abiyyusyabirin@gmail.com>
2024-06-23 20:30:34 +0700

    nambah terraform

---
+++ main.tf
@@ -0,0 +1,127 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA3FLD5TGMUDJNZ45R"
+  secret_key = "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"
+}
+
+// To Generate Private Key
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+}
+
+// Create Key Pair for Connecting EC2 via SSH
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+// Save PEM file locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+
+  provisioner "local-exec" {
+    command = "chmod 400 ${var.key_name}"
+  }
+}
+
+# Create a security group
+resource "aws_security_group" "sg_ec2" {
+  name        = "sg_ec2"
+  description = "Security group for EC2"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "icareer" {
+  ami                    = "ami-08a0d1e16fc3f61ea"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+
+  tags = {
+    Name = "icareer"
+  }
+  
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+
+  provisioner "local-exec" {
+    command = "touch dynamic_inventory.ini"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'EC2 instance is ready.'"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ec2-user"
+      private_key = tls_private_key.rsa_4096.private_key_pem
+    }
+  }
+}
+
+data "template_file" "inventory" {
+  template = <<-EOT
+    [ec2_instances]
+    ${aws_instance.icareer.public_ip} ansible_user=ubuntu ansible_private_key_file=${path.module}/${var.key_name}
+    EOT
+}
+
+resource "local_file" "dynamic_inventory" {
+  depends_on = [aws_instance.icareer]
+
+  filename = "dynamic_inventory.ini"
+  content  = data.template_file.inventory.rendered
+
+  provisioner "local-exec" {
+    command = "chmod 400 ${local_file.dynamic_inventory.filename}"
+  }
+}
+
+# resource "null_resource" "run_ansible" {
+#   depends_on = [local_file.dynamic_inventory]
+
+#   provisioner "local-exec" {
+#     command = "ansible-playbook -i dynamic_inventory.ini deploy-app.yml"
+#     working_dir = path.module
+#   }
+# }
