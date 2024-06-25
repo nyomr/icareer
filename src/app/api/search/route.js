17ab7206404c41b4c71b9cd78036e5cf20253598
Nyoman Richardo <99974514+nyomr@users.noreply.github.com>
2024-06-25 23:45:26 +0700

    Update route.js

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -1,29 +1,3 @@
-// import { NextResponse } from 'next/server';
-// import connect from '../../../db';
-// import Post from '../../../models/Post';
-
-// export const GET = async (request) => {
-//     try {
-//         await connect();
-//         const { searchParams } = new URL(request.url);
-//         const query = searchParams.get('query');
-
-//         if (!query) {
-//             return new NextResponse(JSON.stringify({ error: 'Query parameter is required' }), { status: 400 });
-//         }
-
-//         const post = await Post.findOne({ title: { $regex: query, $options: 'i' } });
-
-//         if (!post) {
-//             return new NextResponse(JSON.stringify({ error: 'Post not found' }), { status: 404 });
-//         }
-
-//         return new NextResponse(JSON.stringify(post), { status: 200 });
-//     } catch (error) {
-//         return new NextResponse(`Error in fetching posts: ${error.message}`, { status: 500 });
-//     }
-// };
-// src/app/api/posts/route.js
 import { NextResponse } from 'next/server';
 import connect from '../../../../db';
 import Post from '../../../../models/Post';
@@ -38,7 +12,7 @@ export const GET = async (request) => {
             // Handle search
             const post = await Post.findOne({ title: { $regex: query, $options: 'i' } });
             if (!post) {
-                return new NextResponse(JSON.stringify({ error: 'Post not found' }), { status: 404 });
+                return new NextResponse(JSON.stringify({ error: 'Pekerjaan tidak ditemukan' }), { status: 404 });
             }
             return new NextResponse(JSON.stringify(post), { status: 200 });
         } else {
