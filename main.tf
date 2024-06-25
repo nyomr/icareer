2ead33f4970ea5b9749d61657f87b97235518855
AbiyyuSyabirin <89232989+AbiyyuSyabirin@users.noreply.github.com>
2024-06-25 15:58:11 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,18 +19,17 @@ resource "tls_private_key" "rsa_4096" {
 }
 
 variable "key_name" {
-  description = "icareer_key"
-  default     = "icareer_key"  
+  description = "terraform-key"
+  default     = "terraform-key"  
 }
 
 resource "aws_key_pair" "service_key_pair" {
-  key_name   = var.key_name
   public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
 resource "local_file" "private_key" {
   content = tls_private_key.rsa_4096.private_key_pem
-  filename = "${var.key_name}.pem"
+  filename = var.key_name
 }
 
 resource "aws_security_group" "allow_http_ssh" {
@@ -58,26 +57,17 @@ resource "aws_security_group" "allow_http_ssh" {
   }
 }
 
-resource "aws_instance" "i-career" {
-  ami           = "ami-01b799c439fd5516a"
-  instance_type = "t3.micro"
-  key_name      = aws_key_pair.service_key_pair.key_name
+resource "aws_instance" "public_instance" {
+  ami = "ami-01b799c439fd5516a"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.service_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
 
   tags = {
-    Name = "i-career"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update",
-      "sudo apt upgrade -y",
-      "docker --version",
-      "docker ps"
-      ]
+    Name = "icareer"
   }
 }
 
 output "instance_ip" {
-  value = aws_instance.i-career.public_ip
+  value = aws_instance.public_instance.public_ip
 }
