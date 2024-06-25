aaeee23031015241f09e1e84971f02d9c300c18b
AbiyyuSyabirin <89232989+AbiyyuSyabirin@users.noreply.github.com>
2024-06-25 13:11:30 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "allow_http_ssh" {
 }
 
 resource "aws_instance" "public_instance" {
-  ami = "ami-0eac975a54dfee8cb"
+  ami = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
   key_name = aws_key_pair.service_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
