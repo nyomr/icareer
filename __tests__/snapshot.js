b91da1070c5e02c2cdb5b695f6eff91391303563
Nyoman Richardo <99974514+nyomr@users.noreply.github.com>
2024-06-25 23:43:37 +0700

    Update snapshot.js

--- __tests__/snapshot.js
+++ __tests__/snapshot.js
@@ -1,6 +1,5 @@
 import { render } from '@testing-library/react';
 import Home from '../src/app/page';
-import App from '@/app/app/page';
  
 describe('Home Page', () => {
   it('renders homepage unchanged', () => {
