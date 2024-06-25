feca9d0b88bb863210f83138ff3e3e4709ddc965
AbiyyuSyabirin <89232989+AbiyyuSyabirin@users.noreply.github.com>
2024-06-25 13:55:00 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = ami-01b799c439fd5516a
-  instance_type = t2.micro
-  key_name      = key-ec2
+  ami           = "ami-01b799c439fd5516a"
+  instance_type = "t2.micro"
+  key_name      = "key-ec2"
   tags = {
     Name = "i-career"
   }
