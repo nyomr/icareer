fc0d10ae7e17a163cd30a0705acac46669f54f1c
Muhammad Rivan Ferdiansyah <99782584+F3RD1N@users.noreply.github.com>
2024-06-25 19:16:25 +0700

    Update file.sh

--- file.sh
+++ file.sh
@@ -5,3 +5,5 @@ sudo apt-get install docker.io -y
 sudo systemctl start docker
 sudo chmod 666 /var/run/docker.sock
 sudo systemctl enable docker
+docker pull psoakun/fp-pso:latest
+docker run -d -p 3000:3000 psoakun/fp-pso:latest
